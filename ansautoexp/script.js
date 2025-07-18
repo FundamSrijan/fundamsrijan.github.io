0a0050c1c6befa616c423f2156d51c469a8689d7
FUNDAM <srijancreates832@gmail.com>
2025-07-18 21:30:34 +0530

    Update script.js

--- ansautoexp/script.js
+++ ansautoexp/script.js
@@ -2021,8 +2021,8 @@ ogSkin.addEventListener("load",() =>{
     
     config.style.display = "block";
     invalidWarning.style.display = "block";
-    invalidWarning.style.color = "yellow";
-    invalidWarning.style.webkitTextFillColor = "yellow";
+    // invalidWarning.style.color = "yellow";
+    // invalidWarning.style.webkitTextFillColor = "yellow";
     invalidWarning.content = "Warning : HD Skins aren't Currently Supported for Actions & Stuff expressions";
     
     
@@ -2036,8 +2036,8 @@ ogSkin.addEventListener("load",() =>{
     
     config.style.display = "none";
     invalidWarning.style.display = "block";
-    invalidWarning.style.color = "red";
-    invalidWarning.style.webkitTextFillColor = "red";
+    // invalidWarning.style.color = "red";
+    // invalidWarning.style.webkitTextFillColor = "red";
     invalidWarning.content = "Invalid Skin Uploaded";
     
   };
